e875bfac16acb5b51c1406c6ed2287290b0b7ae9
Aronn Laurel <aronngrant97@gmail.com>
2025-05-16 19:15:12 -0700

    Added Contact form (skeleton)

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Header from "./components/Header"
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Skill from "./components/Skill"
+import Work from "./components/Work"
+import Contact from "./components/Contact"
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
         <Hero />
         <About />
         <Skill />
+        <Work />
+        <Contact />
         
       </main>
       
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,3 +1,6 @@
+/* Components */
+import ProjectCard from "./ProjectCard"
+
 const works = [
   {
     imgSrc: '/images/project-1.jpg',
@@ -36,3 +39,31 @@ const works = [
     projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
   },
 ];
+
+const Work = () => {
+  return (
+    <section id="work" className="section">
+      <div className="container">
+
+        <h2 className="headline-2 mb-8">
+          My Portfolio Highlights
+        </h2>
+
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
+          {works.map(({ imgSrc, title, tags, projectLink }, key) => (
+            <ProjectCard 
+              key={key}
+              imgSrc={imgSrc}
+              title={title}
+              tags={tags}
+              projectLink={projectLink}
+            />
+          ))}
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Work
