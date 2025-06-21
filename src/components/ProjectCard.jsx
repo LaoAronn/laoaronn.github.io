b4f34373694f052a6b22d5c9fa43a2a2a94212ae
Aronn Laurel <aronngrant97@gmail.com>
2025-06-21 15:55:15 -0700

    Project Layout Design final

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,45 +6,31 @@ const ProjectCard = ({
     title,
     tags,
     projectLink,
+    desc,
     classes
 }) => {
     return (
         <div className={"relative p-4 rounded-2xl bg-zinc-800 hover:bg-zinc-700/50 active:bg-zinc-700/60 ring-1 ring-inset ring-zinc-50/5 transition-colors " + classes}>
             
-            <div className="">
+            <a className="rounded-lg p-4 transition-all flex flex-col items-stretch justify-start gap-4" href={projectLink}>
+                <h3 className="font-mono font-bold text-lg title-1">
+                    {title}
+                </h3>
+
+                <div className="flex flex-wrap justify-center items-center gap-2">
+                    {tags.map((label, key) => (
+                        <span key={key} className="h-8 text-sm text-zinc-400 bg-zinc-50/5 grid items-center px-3 rounded-lg">
+                            {label}
+                        </span>
+                    ))}
+                </div>
+
+                <div className="mb-2">
+                    {desc}
+                </div>
                 
-                <a className="rounded-lg p-4 bg-black/5 border-2 border-solid border-black/5 transition-all hover:bg-black/10 flex flex-col items-stretch justify-start gap-2" href={projectLink}>
-                    <h3 className="title-1 text-left mb-4">
-                        {title}
-                    </h3>
-
-                    <div className="flex flex-wrap items-center gap-2">
-                        {tags.map((label, key) => (
-                            <span key={key} className="h-8 text-sm text-zinc-400 bg-zinc-50/5 grid items-center px-3 rounded-lg">
-                                {label}
-                            </span>
-                        ))}
-                    </div>
-
-                    <div className="flex items-center justify-between ml-5">
-                        Content
-
-                        <div className="text-right pr-2">
-                            <span className="material-symbols-rounded" aria-hidden="true">
-                                arrow_outward
-                            </span>
-                        </div>
-
-                    </div>
-                    
-                </a>
-                    
-            
-
-
-                
-
-            </div>
+            </a>
+    
 
         </div>
     )
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,32 +5,38 @@ const works = [
   {
     title: 'Movie Review Website 🍿',
     tags: ['Full-Stack Development', 'Web Server'],
-    projectLink: 'https://github.com/LaoAronn/Movie-Website'
+    projectLink: 'https://github.com/LaoAronn/Movie-Website',
+    desc: 'Movie review platform with user login, rating, and review system'
   },
   {
     title: 'Game Platform Systems 🎮',
     tags: ['Database', 'System Design'],
-    projectLink: 'https://github.com/LaoAronn/Game-Platform-Systems-'
+    projectLink: 'https://github.com/LaoAronn/Game-Platform-Systems-',
+    desc: 'Database for organizing game info, player reviews, and news using relational schema'
   },
   {
     title: 'CookAI 🥘',
     tags: ['UI Development', 'API', 'AI/ML'],
-    projectLink: 'https://devpost.com/software/cook-ai'
+    projectLink: 'https://devpost.com/software/cook-ai',
+    desc: 'Generates recipes from ingredient photos with AI suggestions and responsive UI'
   },
   {
-    title: 'Pacman Game 🕹️',
+    title: 'Pac-Man Clone 🕹️',
     tags: ['Web Development', 'Game Development'],
-    projectLink: 'https://github.com/LaoAronn/Bootleg-Pacman'
+    projectLink: 'https://github.com/LaoAronn/Bootleg-Pacman',
+    desc: 'Interactive Pac-Man game with a twist of difficulty levels and obstacle mechanics'
   },
   {
     title: 'UBC Men’s Basketball Team Roster Log 🏀',
     tags: ['GUI Application'],
-    projectLink: 'https://github.com/LaoAronn/UBC-mbb-log'
+    projectLink: 'https://github.com/LaoAronn/UBC-mbb-log',
+    desc: 'Application managing and saving basketball team rosters with file I/O support'
   },
   {
     title: 'Digit Mastermind #️⃣',
     tags: ['Game Development'],
-    projectLink: 'https://github.com/LaoAronn/Digit-Mastermind'
+    projectLink: 'https://github.com/LaoAronn/Digit-Mastermind',
+    desc: 'Code-breaking game with real-time terminal input and feedback loop.'
   },
 ];
 
@@ -44,12 +50,13 @@ const Work = () => {
         </h2>
 
         <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-4 gap-x-4 gap-y-5">
-          {works.map(({ title, tags, projectLink }, key) => (
+          {works.map(({ title, tags, projectLink, desc }, key) => (
             <ProjectCard 
               key={key}
               title={title}
               tags={tags}
               projectLink={projectLink}
+              desc={desc}
               classes="reveal-up"
             />
           ))}
