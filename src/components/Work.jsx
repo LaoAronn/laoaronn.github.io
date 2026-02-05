36583314ad598a8c5954c47ab936654f37c70bed
Aronn Laurel <aronngrant97@gmail.com>
2026-02-05 11:17:21 -0800

    Testing Project header

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -95,13 +95,16 @@ const Work = () => {
       <div className="container">
 
         {/** Project Header */}
-        <div class="inline-flex items-center justify-center w-full reveal-up pb-15 ">
+        <div class="relative inline-flex items-center justify-center w-full reveal-up pb-15">
           <hr class="w-full h-1 my-8 bg-gray-100 border-0 rounded-sm dark:bg-zinc-800"/>
 
-          <span class="absolute px-3 headline-2 -translate-x-1/2 left-1/2 dark:text-white dark:bg-zinc-900">
+          <span class="absolute px-3 headline-2 -translate-x-1/2 left-1/2 
+            dark:text-white dark:bg-zinc-900 
+            bg-white z-10">
             My Projects
           </span>
         </div>
+
         
         
         <div className="flex justify-center pb-15  reveal-up">
