322b56bb34a65382134d19f6506f2c42320e363b
Aronn Laurel <65562628+LaoAronn@users.noreply.github.com>
2026-04-23 16:35:36 -0700

    Added Verzena work videos Removed Work Experience header

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -28,16 +28,6 @@ const About = () => {
 
       <div className="container px-4 sm:px-6">
 
-        {/** Work Header */}
-        <div className="inline-flex items-center justify-center w-full reveal-up pb-10 sm:pb-15">
-          <hr className="w-full h-[1px] my-6 sm:my-8 bg-gray-100 border-0 rounded-sm dark:bg-zinc-800"/>
-
-          <span className="absolute px-3 headline-2 text-sm sm:text-base md:text-lg 
-            -translate-x-1/2 left-1/2 dark:text-white dark:bg-zinc-900">
-            My Experience
-          </span>
-        </div>
-
         {/** Work Experience List */}
         {workExperience.map(({ workid, company, position, period, desc }, key) => {
 
