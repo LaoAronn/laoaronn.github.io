ccb8be51fd9da41c7f1f2b3bd5daa62375975cae
Aronn Laurel <65562628+LaoAronn@users.noreply.github.com>
2026-09-11 19:00:24 -0700

    fixed build error (i think)

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 */
 import About from "../pages/About";
 import Works from "../pages/Works";
-import Projects from "../pages/projects";
+import Projects from "../pages/Projects";
 
 const Footer = () => {
   const [vancouverTime, setVancouverTime] = useState("");
